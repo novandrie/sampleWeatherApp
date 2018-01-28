77519ef9c91c3e7e25b813bb1abd35beb198d819
Gabriel Lefundes <gabriel.5489@gmail.com>
2018-01-28 12:50:27 -0300

    added loading spinner; changed font; places Salvador as first in list; cleaner selection method

--- main.js
+++ main.js
@@ -7,27 +7,29 @@ $.getJSON("resources/cidades-bahia.json", function(data){
     // Carrega lista de cidades de arquivo jSON para menu dropdown 
     $.each(data.cidades, function(){
         cidades = data.cidades;
-        $("#menuCidades").append("<li><a href='#'>"+this+"</a></li>");
+        $("#menuCidades").append("<option class='select-option'>"+this+"</option>");
     });
-
-    // Seleção de cidade
-    $("#menuCidades li a").click(function(){
-        // Coloca nome da cidade selecionada no botão do menu dropdown
-        $(this).parents(".dropdown").find(".cidade").html($(this).text());
-        cidadeSelecionada = $(this).text();
-        // Busca de informação meteorologica via API apixu
-        $.getJSON('https://api.apixu.com/v1/forecast.json?key='+APIKEY+'&q='+cidadeSelecionada+'&days=4')
-            // Caso sucesso na chamada da API, chamar função para estruturar e exibir dados
-            .done(function(data){
-                $('.error-message').hide();
-                parseWeatherData(data);
-            })
-            // Caso contrário, exibir mensagem de que a cidade escolhida não se encontra na lista
-            .fail(function(data){
-                clearWeatherData();
-                $('.error-message').show()
-            })
-    });
+    // Carrega dados para primeiro elemento(sendo mostrado já na lista)
+    selectCity(cidades[0])
 
 });
 
+function selectCity(cidade){
+    $(".spinner").show()
+    cidadeSelecionada = cidade;
+    // Busca de informação meteorologica via API apixu
+    $.getJSON('https://api.apixu.com/v1/forecast.json?key='+APIKEY+'&q='+cidadeSelecionada+'&days=4')
+        // Caso sucesso na chamada da API, chamar função para estruturar e exibir dados
+        .done(function(data){
+            $('.error-message').hide();
+            parseWeatherData(data);
+            $(".spinner").hide()
+        })
+        // Caso contrário, exibir mensagem de que a cidade escolhida não se encontra na lista
+        .fail(function(data){
+            clearWeatherData();
+            $('.error-message').show()
+            $(".spinner").hide()
+        });
+
+}
